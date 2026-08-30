cc5aff30ba57390e3d7a3dac7b623ba8ebc7c99c
MYNAMEISJEFF482 <45806318+caden4314@users.noreply.github.com>
2026-08-30 00:52:37 -0500

    Start LGHS 0.5 controller package

---
+++ controller/lghs/__init__.py
@@ -0,0 +1,8 @@
+"""LGHS controller core package.
+
+0.5.x moves controller state and protocol logic out of executable scripts into
+importable, testable modules. Executable compatibility shims remain in place
+while the migration is staged.
+"""
+
+__version__ = "0.5.0"
